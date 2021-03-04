14239ae28c94e62971af7a531c4ecb72690f6e2a
JIHYEJANELEE <leexxjane@gmail.com>
2021-03-04 10:49:06 +0900

    feat: error

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,12 @@
-for i in range(1,100+1):
-	print(i, end='\t')
 
-print('')
+for i in range(1,100+1):
+        if i%3==0:
+		print_('fizz',end='\t')
+	elif i%5==0:
+		print_('buzz',end='\t')
+	elif i%15==0:
+		print_('fizzbuzz',end='\t')
+	else:
+		print_(i,end='\t')	
+
 
