75d887c98c74e0daadc3bd94f8f102e6dbfd2055
JIHYEJANELEE <leexxjane@gmail.com>
2021-03-04 11:47:01 +0900

    chagne

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,11 @@
 def print_(obj):
     print(obj, end='\t')
 
-for i in range(1,100+1):
+for i in range(1,300+1):
+    if i%3==0:
     if i%15==0:
-        print_('피즈버즈')
+        print_('fizzbuzz')
+    elif i%3==0:
+        print_('fizz')
     elif i%5==0:
-        print_('BUZZ')
-    else:
-        print_(i)
-
-print('')
+        print_('buzz')
