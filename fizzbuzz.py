d2c76c741b052f3b6b7b62c28015529209a695c3
JIHYEJANELEE <leexxjane@gmail.com>
2021-03-04 10:56:49 +0900

    feat: recreate fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,14 @@
+def print_(obj):
+    print(obj, end='\t')
 
 for i in range(1,100+1):
-        if i%3==0:
-		print_('fizz',end='\t')
-	elif i%5==0:
-		print_('buzz',end='\t')
-	elif i%15==0:
-		print_('fizzbuzz',end='\t')
-	else:
-		print_(i,end='\t')	
+    if i%15==0:
+        print_('fizzbuzz')
+    elif i%3==0:
+        print_('fizz')
+    elif i%5==0:
+        print_('buzz')
+    else:
+        print_(i)
 
-
+print('')
